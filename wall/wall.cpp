ff6c6c86210bc82c9086b644bb99b7385e53e41f
Marie Chantal Tan <mariechantaltan@gmail.com>
2016-07-13 23:37:40 -0700

    Update wall.cpp

--- wall/wall.cpp
+++ wall/wall.cpp
@@ -45,37 +45,6 @@ void print_delay_17(string text)
     return;
 }
 
-void opening()
-{
-    timer(2);
-    cout << endl << endl << endl;
-    print_delay_10("I would build a great wall.");
-    print_delay_10("And nobody builds walls better than me,");
-    print_delay_10("believe me.");
-    print_delay_10("And I'll build them very inexpensively.");
-    print_delay_10("I will build a great,");
-    cout << left << setw(8) << " " << right << setw(8) << "great" << endl;
-    timer(2);
-    cout << left << setw(7) << " " << right << setw(7) << "wall" << endl;
-    timer(2);
-    print_delay_10("on our southern border");
-    print_delay_10("and I'll have Mexico pay for that wall.");
-    print_delay_10("-Donald Trump");
-    return;
-}
-
-void title()
-{
-    cout << endl << endl << endl;
-    print_text_30("██╗    ██╗ █████╗ ██╗     ██╗     ");
-    print_text_30("██║    ██║██╔══██╗██║     ██║     ");
-    print_text_30("██║ █╗ ██║███████║██║     ██║     ");
-    print_text_30("██║███╗██║██╔══██║██║     ██║     ");
-    print_text_30("╚███╔███╔╝██║  ██║███████╗███████╗");
-    print_text_30(" ╚══╝╚══╝ ╚═╝  ╚═╝╚══════╝╚══════╝");
-    return;
-}
-
 void main_menu()
 {
     cout << endl << endl << endl;
@@ -90,24 +59,6 @@ void main_menu()
     return;
 }
 
-void day_zero()
-{
-    timer(2);
-    cout << endl << endl << endl;
-    print_text_10("  ^    ^    ^       ^  ");
-    print_text_10(" |D|  |A|  |Y|     |0| ");
-    print_text_10("<___><___><___>   <___>");
-    return;
-}
-
-void chapter_zero()
-{   
-    cout << endl << endl << endl;
-    print_text_10("╔╦═╗ ╦ ╔═╗  ╦═╗ ╦ ╔═╗ ╔═╗");
-    print_text_10(" ║ ║ ║ ║╣   ╠╦╝ ║ ╚═╗ ║╣ ");
-    print_text_10("═╩═╝ ╩ ╚═╝  ╩╚═ ╩ ╚═╝ ╚═╝");
-    return;
-}
 
 void narration_1()
 {
